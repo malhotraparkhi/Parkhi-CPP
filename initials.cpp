d195ac8a682443d7f15b71b28653e28216786a78
Parkhi Malhotra <65663646+malhotraparkhi@users.noreply.github.com>
2020-10-25 15:36:22 +0530

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,60 @@
+//I will start with basic C++ programs
+#include<iostream>
+int main()
+{
+  //This is the first initial
+  std::cout<<"PPPP\n";
+  std::cout<<"P   P\n";
+  std::cout<<"P   P\n";
+  std::cout<<"PPPP\n";
+  std::cout<<"P\n";
+  std::cout<<"P\n";
+  std::cout<<"P\n";
+  std::cout<<"\n";
+//This is the second initial
+std::cout<<"  A  \n";
+std::cout<<" A A \n";
+std::cout<<"A   A\n";
+std::cout<<"AAAAA\n";
+std::cout<<"A   A\n";
+std::cout<<"A   A\n";
+std::cout<<"A   A\n";
+std::cout<<"\n";
+//This is the third initial
+  std::cout<<"RRRR\n";
+  std::cout<<"R   R\n";
+  std::cout<<"R   R\n";
+  std::cout<<"RRRR\n";
+  std::cout<<"R R\n";
+  std::cout<<"R  R\n";
+  std::cout<<"R   R\n";
+  std::cout<<"\n";
+//This is the fourth initial
+std::cout<<"K   K\n";
+std::cout<<"K  K\n";
+std::cout<<"K K\n";
+std::cout<<"KK\n";
+std::cout<<"K K\n";
+std::cout<<"K  K\n";
+std::cout<<"K   K\n";
+std::cout<<"\n";
+//This is the fifth initial
+  std::cout<<"H   H\n";
+  std::cout<<"H   H\n";
+  std::cout<<"H   H\n";
+  std::cout<<"HHHHH\n";
+  std::cout<<"H   H\n";
+  std::cout<<"H   H\n"; 
+  std::cout<<"H   H\n";
+  std::cout<<"\n"; 
+//This is the sixth initial
+std::cout<<"IIIII\n";
+std::cout<<"I   I\n";
+std::cout<<"I   I\n";
+std::cout<<"I   I\n";
+std::cout<<"I   I\n";
+std::cout<<"I   I\n";
+std::cout<<"I   I\n";
+std::cout<<"\n"; 
+//This is my name
+}
