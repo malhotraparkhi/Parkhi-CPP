df4f468edf6521e3b5bf57af65c82edb8f0e8ac3
Parkhi Malhotra <65663646+malhotraparkhi@users.noreply.github.com>
2020-10-29 01:14:38 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,22 @@
+/*Write a program to calculate and print a simple list of squares from 0 to 9*/
+
+#include <iostream>
+int main() 
+{
+  int i = 0;
+  int square = 0;
+
+  while (i<10)
+  {
+    square =i*i;
+    std::cout<<i<<" "<<square<<"\n";
+    i++;
+  }
+  
+  
+  
+  
+  
+  
+  
+}
